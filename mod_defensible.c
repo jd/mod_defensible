a37a93ccb97769eb236f4af6c63907afd4b0cebf
Julien Danjou <julien@danjou.info>
2012-04-03 15:46:43 +0200

    Update copyright years
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- mod_defensible.c
+++ mod_defensible.c
@@ -1,7 +1,7 @@
 /*
  * mod_defensible.c - Forbid page access using DNSBL
  *
- * Copyright © 2007 Julien Danjou <julien@danjou.info>
+ * Copyright © 2007-2012 Julien Danjou <julien@danjou.info>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
